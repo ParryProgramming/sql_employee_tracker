438feded92c5d6bfc95343fce69c6e7ee55b502e
Parry <parryprogramming@gmail.com>
2024-05-06 21:42:41 -0400

    added starter code

---
+++ index.js
@@ -0,0 +1,64 @@
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+const pool = require("./db/connection");
+const { Pool } = require("undici-types");
+
+init();
+
+// Display logo text, load main prompts
+function init() {
+  const logoText = logo({ name: "City of Pawnee" }).render();
+  const pool = new Pool({
+    host:
+    
+  })
+
+  console.log(logoText);
+
+  loadMainPrompts();
+}
+
+function loadMainPrompts() {
+  prompt([
+    // TODO- Create first question user will see- "What would you like to do?"
+  ]).then((res) => {
+    // TODO- Create a variable to store the user's choice
+    // TODO- Create a switch statement to call the appropriate function depending on what the user chose
+  });
+}
+
+// TODO- Create a function to View all employees
+function viewEmployees() {}
+
+// BONUS- Create a function to View all employees that belong to a department
+
+// BONUS- Create a function to View all employees that report to a specific manager
+
+// BONUS- Create a function to Delete an employee
+
+// TODO- Create a function to Update an employee's role
+
+// BONUS- Create a function to Update an employee's manager
+
+// TODO- Create a function to View all roles
+
+// TODO- Create a function to Add a role
+
+// BONUS- Create a function to Delete a role
+
+// TODO- Create a function to View all deparments
+
+// TODO- Create a function to Add a department
+
+// BONUS- Create a function to Delete a department
+
+// BONUS- Create a function to View all departments and show their total utilized department budget
+
+// TODO- Create a function to Add an employee
+
+// Exit the application
+function quit() {
+  console.log("Goodbye!");
+  process.exit();
+}
