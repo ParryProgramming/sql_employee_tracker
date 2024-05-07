37854e553cf36f1dd51c66a4b4697d1438a3273b
Parry <parryprogramming@gmail.com>
2024-05-06 21:50:44 -0400

    added routing to index.js and prompt questions

--- index.js
+++ index.js
@@ -2,34 +2,252 @@ const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
 const db = require("./db");
 const pool = require("./db/connection");
-const { Pool } = require("undici-types");
 
 init();
-
+ 
 // Display logo text, load main prompts
 function init() {
   const logoText = logo({ name: "City of Pawnee" }).render();
-  const pool = new Pool({
-    host:
-    
-  })
 
+ 
   console.log(logoText);
+
 
   loadMainPrompts();
 }
 
 function loadMainPrompts() {
   prompt([
-    // TODO- Create first question user will see- "What would you like to do?"
-  ]).then((res) => {
-    // TODO- Create a variable to store the user's choice
-    // TODO- Create a switch statement to call the appropriate function depending on what the user chose
-  });
-}
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'option',
+        choices: ['Add', 'View', 'Update', 'Delete', 'Exit' ]    
+    }
 
+  ]).then((res) => {
+    // TODO- Create a switch statement to call the appropriate function depending on what the user chose
+   
+    switch (res.option) {
+        case 'Add': 
+            addPrompt();
+            break;
+        case 'View':
+            viewPrompt();
+            break;
+        case 'Update':
+            updatePrompt();
+            break;
+        case 'Delete':
+            deletePrompt();
+            break;
+        case 'Exit':
+            quit();
+            break;
+        default:
+            console.log("Invalid Option");
+        }
+    })
+  };
+  function addPrompt(){
+    prompt([
+        {
+            type:'list',
+            message:'What do you want to add to?',
+            name:'add',
+            choices: ['Department', 'Role', 'Employee']
+        }
+    ]).then((res) => {
+        switch (res.add) {
+            case 'Department':
+                addDepartment();
+                break;
+            case 'Role':
+                addRole();
+                break;
+            case 'Employee':
+                addEmployee();
+                break;
+            default:
+                console.log("Invalid Option");
+        }
+    })
+  };
+  function viewPrompt() {
+    prompt([
+        {
+        type:'list',
+        message: 'What would you like to do?',
+        name: 'view',
+        choices: ['View all employees', 'View all roles', 'View all departments']
+        }
+    ]).then((res) => {
+        switch(res.view) {
+        case 'View all employees':
+            viewAllEmployees();
+            break;
+        case 'View all roles':
+            viewAllRoles();
+            break;
+        case 'View all departments':
+            viewAllDepartments();
+            break;
+        default:
+            console.log("Invalid option");
+        }
+    })
+  };
+  function updatePrompt() {
+    prompt ([
+        {
+        type: 'list',
+        message: 'What do you want to update?',
+        name: 'update',
+        choices: ['Update employee', 'Update role', 'Update department']
+        }
+    ]).then(res => {
+        switch (res.update) {
+            case 'Update employee':
+                updateEmployee();
+                break;
+            case 'Update role':
+                updateRole();
+                break;
+            case 'Update department':
+                updateDepartment()
+                break;
+            default:
+                console.log("Invalid Option");
+
+        }
+    })
+  };
+function deletePrompt() {
+  prompt([
+    {
+        type: 'list',
+        message: 'What do you want to delete?',
+        name: 'delete',
+        choices: ['Employee', 'Role', 'Department']    
+    }
+
+  ]).then((res) => {    
+    switch (res.delete) {
+        case 'Employee': 
+            deleteEmployee();
+            break;
+        case 'Role':
+            deleteRole();
+            break;
+        case 'Department':
+            deleteDepartment();
+            break;
+            default:
+            console.log("Invalid Option");
+        }
+    })
+  };
 // TODO- Create a function to View all employees
-function viewEmployees() {}
+
+const viewAllEmployees = async ()=> {
+    let { rows } = await db.findAllEmployees();
+    console.log('\n');
+    console.table(rows);
+    loadMainPrompts();
+};
+const viewAllRoles = async ()=> {
+    let { rows } = await db.findAllRoles();
+    console.log('\n');
+    console.table(rows);
+    loadMainPrompts();
+};
+const viewAllDepartments = async ()=> {
+    let { rows } = await db.findAllDepartments();
+    console.log('\n');
+    console.table(rows);
+    loadMainPrompts();
+};
+  const addDepartment = async () => {
+    let {newDepartment} = await prompt([
+        {
+            name: 'newDepartment',
+            message: 'What is the department called?'            
+        }
+    ])
+let { rows } = await db.inputDepartment(newDepartment);
+console.log("Department added");
+  };
+
+  const addRole = async () => {    
+        const departments = await fetchDepartments();
+    let {newRole, newSalary, department} = await prompt ([
+        {
+            name: 'newRole',
+            message: 'What is the role called?'
+        },
+        {
+            name: 'newSalary',
+            message: 'How much does this role make?'
+        },
+        {
+            type: 'list',
+            name: 'department',
+            message: 'What department is this role in?',
+            choices: departments
+        }           
+    ])        
+    let { rows } = await db.inputRole(newRole, newSalary, department);
+    console.log("Role added");
+  }
+  const addEmployee = async () => {
+    const roles = await fetchRoles(); 
+    
+    let {newEmployee} = await prompt ([
+
+        {
+            name: 'newFirst',
+            message: 'What is the new employees first name?'
+        },
+        {
+            name: 'newLast',
+            message: 'What is the new employees last name?'
+        },
+        {
+            type: 'list',
+            name: 'newEmpRole',
+            message: 'What role is the new employee in?',
+            choices: roles
+        }
+    ])
+    let { rows } = await db.inputEmployee(newEmployee);
+    console.log("Employee added");
+  };
+  function fetchDepartments() {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT department_name FROM department', (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+            if (results.rows.length > 0) {
+          resolve(results.rows.map(row => row.department_name));
+        }
+    }
+    });
+});
+}
+function fetchRoles() {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT title FROM roles', (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+            if (results.rows.length > 0) {
+          resolve(results.rows.map(row => row.title));
+        }
+        }
+      });
+    });
+  }
 
 // BONUS- Create a function to View all employees that belong to a department
 
