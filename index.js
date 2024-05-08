cb1e274af669d2f6182163ecfd3e825414802139
Parry <parryprogramming@gmail.com>
2024-05-07 23:35:19 -0400

    edited index.js switch statements and added comments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { prompt } = require("inquirer");
 
 init();
 
-// Display logo text, load main prompts
+// displays logo text and loads main prompts
 function init() {
     const logoText = logo({ name: "City of Pawnee" }).render();
 
@@ -14,40 +14,47 @@ function init() {
 }
 
 function loadMainPrompts() {
+
     prompt([
         {
             type: 'list',
             name: 'start',
             message: 'What would you like to do?',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee','Quit']
+            choices: ['View All Employees', 'View Employee Roles', 'View All Employees By Department', 'Add Employee', 'Add Employee Role', 'Add Department', 'Update Employee Role']
         }
     ]).then((res) => {
+
         const userChoice = res.start
         switch (userChoice) {
-            case 'View all departments':
-                viewDepartments()
-                break;
-            case 'View all roles':
-                viewRoles()
-                break;
-            case 'View all employees':
+
+            case 'View All Employees':
                 viewEmployees()
                 break;
-            case 'Add a department':
-                addDepartment()
+
+            case 'View All Employee Roles':
+                viewRoles()
                 break;
-            case 'Add a role':
-                addRole();
+
+            case 'View All Employees By Department':
+                viewDepartments()
                 break;
-            case 'Add an employee':
+
+            case 'Add Employee':
                 addEmployee();
                 break;
-            case 'Update an employee':
+
+            case 'Add Employee Role':
+                addRole();
+                break;
+
+            case 'Add Department':
+                addDepartment()
+                break;
+
+            case 'Update Employee Role':
                 updateEmployee();
                 break;
-            case 'Quit':
-                quit();
-                break;
+
         }
     });
 }
@@ -57,10 +64,10 @@ function viewEmployees() {
         .then(({ rows }) => {
             console.table(rows)
         })
-        .then(() =>{
+        .then(() => {
             loadMainPrompts()
         })
-    
+
 }
 const updateEmployee = async () => {
     let { rows } = await db.findAllEmployees()
@@ -68,23 +75,31 @@ const updateEmployee = async () => {
         name: `${first_name} ${last_name}`,
         value: employee_id
     }));
+
+
     let res = await db.findAllRoles()
+
     let list = res.rows
-    const roles = list.map(({role_id, title}) => ({
+
+    const roles = list.map(({ role_id, title }) => ({
         name: title,
         value: role_id
     }))
+
+
     let { employee_id, update_item } = await prompt([
         {
             type: 'list',
             name: `employee_id`,
-            message: 'Which employee are you editing?',
+            message: 'Please choose an employee to edit:',
             choices: employees
         },
+
+
         {
             type: 'list',
             name: 'update_item',
-            message: 'What would you like to update?',
+            message: 'Please choose an item to update:',
             choices: [
                 {
                     name: 'First Name',
@@ -102,32 +117,41 @@ const updateEmployee = async () => {
         },
 
     ])
+
+
     let update_info;
+
     switch (update_item) {
+
+
         case 'first_name':
             let { newFirstName } = await prompt([
                 {
                     name: 'newFirstName',
-                    message: 'What is the new first name?'
+                    message: 'Please enter a new first name:'
                 }
             ])
             update_info = newFirstName;
             break;
+
+
         case 'last_name':
             let { newLastName } = await prompt([
                 {
                     name: 'newLastName',
-                    message: 'What is the new last name?'
+                    message: 'Please enter a new last name:'
                 }
             ])
             update_info = newLastName;
             break;
+
+
         case 'role_id':
             let { newRole } = await prompt([
                 {
                     type: 'list',
                     name: 'newRole',
-                    message: 'What is the new role?',
+                    message: 'Please enter a new role:',
                     choices: roles
                 }
             ])
@@ -135,11 +159,14 @@ const updateEmployee = async () => {
             break;
     }
     await db.updateEmployee(employee_id, update_item, update_info)
+
     console.log('Updated Employee.')
+
     loadMainPrompts()
 }
 
 function viewRoles() {
+
     db.findAllRoles()
         .then(({ rows }) => {
             console.table(rows)
@@ -149,35 +176,43 @@ function viewRoles() {
 }
 
 const addRole = async () => {
+
     let { rows } = await db.findAllDepartments();
+
     const departments = rows.map(({ department_name, department_id }) => ({
         name: department_name,
         value: department_id
     }))
+
+
     let { newRole, newSalary, department } = await prompt([
         {
             type: 'input',
             name: 'newRole',
-            message: 'What is the new role called?'
+            message: 'Please enter new role name:'
         },
         {
             type: 'input',
             name: 'newSalary',
-            message: 'What is the salary for new role?'
+            message: 'Please enter salary for new role:'
         },
         {
             type: 'list',
             name: 'department',
-            message: 'What department does the new role belong to?',
+            message: 'Please enter department for new role:',
             choices: departments
         }
     ]);
+
     await db.inputRole(newRole, newSalary, department)
+
     console.log('Added role.')
+
     loadMainPrompts()
 }
 
 function viewDepartments() {
+
     db.findAllDepartments()
         .then(({ rows }) => {
             console.table(rows)
@@ -187,49 +222,53 @@ function viewDepartments() {
 }
 
 const addDepartment = async () => {
+
     let { newDepartment } = await prompt([
         {
             type: 'input',
             name: 'newDepartment',
-            message: 'What would you like the department to be called?'
+            message: 'Please enter the new department name:'
         }
     ])
+
     await db.inputDepartment(newDepartment);
+
     console.log('New department added.');
+
     loadMainPrompts();
 }
 
 const addEmployee = async () => {
+
     let { rows } = await db.findAllRoles()
-    const roles = rows.map(({role_id, title}) => ({
+
+    const roles = rows.map(({ role_id, title }) => ({
         name: title,
         value: role_id
     }))
-    let {first_name, last_name, role_id} = await prompt([
+
+    let { first_name, last_name, role_id } = await prompt([
         {
             type: 'input',
             name: 'first_name',
-            message: `What is the employee's first name?`
+            message: `Please enter employee's first name:`
         },
         {
             type: 'input',
             name: 'last_name',
-            message: `What is the employee's last name?`
+            message: `Please enter employee's last name:`
         },
         {
             type: 'list',
             name: 'role_id',
-            message: `What is the employee's role?`,
+            message: `Please enter employee's role:`,
             choices: roles
         },
     ])
     await db.inputEmployee(first_name, last_name, role_id)
+
     console.log('Employee added.')
+
     loadMainPrompts();
 }
 
-// Exit the application
-function quit() {
-    console.log("Goodbye!");
-    process.exit();
-}
